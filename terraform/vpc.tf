f98eb76e8ad944cea0c19323a060d22c0568959a
Kyle Evers <kyleevers14@yahoo.com>
2019-04-10 10:02:42 -0400

    add missing sections of vpc terraform

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -50,6 +50,42 @@ resource "aws_network_acl" "public" {
   tags = "${merge(var.tags, map("Name", "Playground Public Subnet ACL"))}"
 }
 
+# The internet gateway for the VPC
+resource "aws_internet_gateway" "playground_igw" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  tags = "${merge(var.tags, map("Name", "Playground IGW"))}"
+}
+
+# Private subnet of the VPC, for database and CyHy commander
+resource "aws_subnet" "playground_private_subnet" {
+ vpc_id = "${aws_vpc.vpc.id}"
+ cidr_block = "10.10.10.0/25"
+ availability_zone = "${var.aws_region}${var.aws_availability_zone}"
+
+ depends_on = [
+   "aws_internet_gateway.playground_igw"
+ ]
+
+ tags = "${merge(var.tags, map("Name", "Playground Private"))}"
+}
+
+# Public subnet of the VPC
+#
+# All traffic from private subnet will route through here
+resource "aws_subnet" "playground_public_subnet" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  # TODO: Maybe make this subnet smaller?
+  cidr_block = "10.10.10.128/25"
+  availability_zone = "${var.aws_region}${var.aws_availability_zone}"
+
+  depends_on = [
+    "aws_internet_gateway.playground_igw"
+  ]
+
+  tags = "${merge(var.tags, map("Name", "Playground Public"))}"
+}
+
 # ACL for the private subnet of the VPC
 resource "aws_network_acl" "playground_private_acl" {
   vpc_id = "${aws_vpc.vpc.id}"
@@ -58,4 +94,18 @@ resource "aws_network_acl" "playground_private_acl" {
   ]
 
   tags = "${merge(var.tags, map("Name", "Playground Private Subnet ACL"))}"
+}
+
+# Security group for the private portion of the VPC
+resource "aws_security_group" "playground_private_sg" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  tags = "${merge(var.tags, map("Name", "Playground Private"))}"
+}
+
+# Security group for the bastion host
+resource "aws_security_group" "playground_bastion_sg" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  tags = "${merge(var.tags, map("Name", "Playground Bastion"))}"
 }
