fc3a8f58a3ae847d96f55eb3977678a9affed9c9
Kyle Evers <kyleevers14@yahoo.com>
2019-04-18 15:14:19 -0400

    add guacamole ec2 terraform file

---
+++ terraform/guacamole_ec2.tf
@@ -0,0 +1,65 @@
+# The guacamole AMI
+data "aws_ami" "guacamole" {
+  filter {
+    name = "name"
+    values = [
+      "guacamole-playground-hvm-*-x86_64-ebs"
+    ]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name = "root-device-type"
+    values = ["ebs"]
+  }
+
+  owners = ["${data.aws_caller_identity.current.account_id}"] # This is us
+  most_recent = true
+}
+
+# The guacamole EC2 instance
+resource "aws_instance" "guacamole" {
+  ami = "${data.aws_ami.guacamole.id}"
+  instance_type = "t3.micro"
+  availability_zone = "${var.aws_region}${var.aws_availability_zone}"
+
+  # This is the private subnet
+  subnet_id = "${aws_subnet.playground_private_subnet.id}"
+  associate_public_ip_address = false
+
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 20
+    delete_on_termination = true
+  }
+
+  vpc_security_group_ids = [
+    "${aws_security_group.playground_private_sg.id}"
+  ]
+
+  user_data_base64 = "${data.template_cloudinit_config.ssh_cloud_init_tasks.rendered}"
+
+  tags = "${merge(var.tags, map("Name", "Guacamole"))}"
+  volume_tags = "${merge(var.tags, map("Name", "Guacamole"))}"
+}
+
+# Provision the guacamole EC2 instance via Ansible
+module "playground_bastion_ansible_provisioner" {
+  source = "github.com/cloudposse/tf_ansible"
+
+  arguments = [
+    "--user=${var.remote_ssh_user}",
+    "--ssh-common-args='-o StrictHostKeyChecking=no'"
+  ]
+  envs = [
+    "host=${aws_instance.guacamole.private_ip}",
+    "bastion_host=${aws_instance.guacamole.public_ip}",
+    "host_groups=guacamole"
+  ]
+  playbook = "../ansible/playbook.yml"
+  dry_run = false
+}
