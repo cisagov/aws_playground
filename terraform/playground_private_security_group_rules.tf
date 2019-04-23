336511f6e27358619fd79aada6dc93412dc7d4d8
Kyle Evers <kyleevers14@yahoo.com>
2019-04-23 16:46:56 -0400

    allow ingress for guacd and guac dashboard but and allow guacd to communicate with the dashboard

--- terraform/playground_private_security_group_rules.tf
+++ terraform/playground_private_security_group_rules.tf
@@ -19,3 +19,36 @@ resource "aws_security_group_rule" "private_ssh_ingress_from_bastion" {
   from_port = 22
   to_port = 22
 }
+
+# Allow guacamole ingress from the guacamole instance
+resource "aws_security_group_rule" "private_guacamole_ingress_from_bastion" {
+  security_group_id = "${aws_security_group.playground_private_sg.id}"
+  type = "ingress"
+  protocol = "tcp"
+  cidr_blocks = [
+    "${aws_instance.playground_bastion.private_ip}/32"
+  ]
+  from_port = 8080
+  to_port = 8080
+}
+
+resource "aws_security_group_rule" "private_guacd_ingress_from_bastion" {
+  security_group_id = "${aws_security_group.playground_private_sg.id}"
+  type = "ingress"
+  protocol = "tcp"
+  source_security_group_id = "${aws_security_group.playground_bastion_sg.id}"
+  from_port = 4822
+  to_port = 4822
+}
+
+# Allow webd egress of webd
+resource "aws_security_group_rule" "private_guacd_egress_to_guacui" {
+  security_group_id = "${aws_security_group.playground_private_sg.id}"
+  type = "egress"
+  protocol = "tcp"
+  cidr_blocks = [
+    "${aws_instance.guacamole.private_ip}/32"
+  ]
+  from_port = 4822
+  to_port = 4822
+}
