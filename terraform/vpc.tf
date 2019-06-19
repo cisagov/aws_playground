4ae680f1a23296fed87301087461cda0c2c87a3e
Kyle Evers <kyleevers14@yahoo.com>
2019-06-19 14:19:49 -0400

    Remove duplicate public subnet

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,104 +1,133 @@
 # The playground VPC
 resource "aws_vpc" "vpc" {
-  cidr_block = "10.10.10.0/24"
+  cidr_block           = "10.10.10.0/24"
   enable_dns_hostnames = true
 
-  tags = "${merge(var.tags, map("Name", "Playground VPC"))}"
-}
-
-# Public subnet of the VPC
-resource "aws_subnet" "public" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  cidr_block = "10.10.10.0/24"
-  availability_zone = "${var.aws_region}${var.aws_availability_zone}"
-
-  depends_on = [
-    "aws_internet_gateway.playground_igw"
-  ]
-
-  tags = "${merge(var.tags, map("Name", "Playground Public Subnet"))}"
+  tags = merge(
+    var.tags,
+    {
+      "Name" = "Playground VPC"
+    },
+  )
 }
 
 # Default route table
 resource "aws_default_route_table" "default_route_table" {
-  default_route_table_id = "${aws_vpc.vpc.default_route_table_id}"
+  default_route_table_id = aws_vpc.vpc.default_route_table_id
 
-  tags = "${merge(var.tags, map("Name", "Playground"))}"
+  tags = merge(
+    var.tags,
+    {
+      "Name" = "Playground"
+    },
+  )
 }
 
 # Route all external traffic through the internet gateway
 resource "aws_route" "route_external_traffic_through_internet_gateway" {
-  route_table_id = "${aws_default_route_table.default_route_table.id}"
+  route_table_id         = aws_default_route_table.default_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = "${aws_internet_gateway.playground_igw.id}"
+  gateway_id             = aws_internet_gateway.playground_igw.id
 }
 
 # ACL for the public subnet of the VPC
 resource "aws_network_acl" "public" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
   subnet_ids = [
-    "${aws_subnet.public.id}"
+    aws_subnet.playground_public_subnet.id,
   ]
 
-  tags = "${merge(var.tags, map("Name", "Playground Public Subnet ACL"))}"
+  tags = merge(
+    var.tags,
+    {
+      "Name" = "Playground Public Subnet ACL"
+    },
+  )
 }
 
 # The internet gateway for the VPC
 resource "aws_internet_gateway" "playground_igw" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
 
-  tags = "${merge(var.tags, map("Name", "Playground IGW"))}"
+  tags = merge(
+    var.tags,
+    {
+      "Name" = "Playground IGW"
+    },
+  )
 }
 
 # Private subnet of the VPC, for database and CyHy commander
 resource "aws_subnet" "playground_private_subnet" {
- vpc_id = "${aws_vpc.vpc.id}"
- cidr_block = "10.10.10.0/25"
- availability_zone = "${var.aws_region}${var.aws_availability_zone}"
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.10.10.128/25"
+  availability_zone = "${var.aws_region}${var.aws_availability_zone}"
 
- depends_on = [
-   "aws_internet_gateway.playground_igw"
- ]
+  depends_on = [aws_internet_gateway.playground_igw]
 
- tags = "${merge(var.tags, map("Name", "Playground Private"))}"
+  tags = merge(
+    var.tags,
+    {
+      "Name" = "Playground Private"
+    },
+  )
 }
 
 # Public subnet of the VPC
 #
 # All traffic from private subnet will route through here
 resource "aws_subnet" "playground_public_subnet" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
+
   # TODO: Maybe make this subnet smaller?
-  cidr_block = "10.10.10.128/25"
+  cidr_block        = "10.10.10.0/25"
   availability_zone = "${var.aws_region}${var.aws_availability_zone}"
 
-  depends_on = [
-    "aws_internet_gateway.playground_igw"
-  ]
+  depends_on = [aws_internet_gateway.playground_igw]
 
-  tags = "${merge(var.tags, map("Name", "Playground Public"))}"
+  tags = merge(
+    var.tags,
+    {
+      "Name" = "Playground Public"
+    },
+  )
 }
 
 # ACL for the private subnet of the VPC
 resource "aws_network_acl" "playground_private_acl" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
   subnet_ids = [
-    "${aws_subnet.playground_private_subnet.id}"
+    aws_subnet.playground_private_subnet.id,
   ]
 
-  tags = "${merge(var.tags, map("Name", "Playground Private Subnet ACL"))}"
+  tags = merge(
+    var.tags,
+    {
+      "Name" = "Playground Private Subnet ACL"
+    },
+  )
 }
 
 # Security group for the private portion of the VPC
 resource "aws_security_group" "playground_private_sg" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
 
-  tags = "${merge(var.tags, map("Name", "Playground Private"))}"
+  tags = merge(
+    var.tags,
+    {
+      "Name" = "Playground Private"
+    },
+  )
 }
 
 # Security group for the bastion host
 resource "aws_security_group" "playground_bastion_sg" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
 
-  tags = "${merge(var.tags, map("Name", "Playground Bastion"))}"
+  tags = merge(
+    var.tags,
+    {
+      "Name" = "Playground Bastion"
+    },
+  )
 }
