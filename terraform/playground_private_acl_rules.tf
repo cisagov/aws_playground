6fd69b7165dd903d8fb5db9c37c6a45d5107b782
Kyle Evers <kyleevers14@yahoo.com>
2019-06-19 14:24:08 -0400

    spacing changed to meet terrafrom 12 syntax

--- terraform/playground_private_acl_rules.tf
+++ terraform/playground_private_acl_rules.tf
@@ -1,48 +1,49 @@
 # Allow egress anywhere via https
 # Needed to pull files from GitHub and external data sources (e.g. usgs.gov)
 resource "aws_network_acl_rule" "playground_private_egress_anywhere_via_https" {
-  network_acl_id = "${aws_network_acl.playground_private_acl.id}"
-  egress = true
-  protocol = "tcp"
-  rule_number = 103
-  rule_action = "allow"
-  cidr_block = "0.0.0.0/0"
-  from_port = 443
-  to_port = 443
+  network_acl_id = aws_network_acl.playground_private_acl.id
+  egress         = true
+  protocol       = "tcp"
+  rule_number    = 103
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 443
+  to_port        = 443
 }
 
 # Allow ingress from anywhere via ephemeral ports
 resource "aws_network_acl_rule" "private_ingress_from_anywhere_via_ephemeral_ports" {
-  network_acl_id = "${aws_network_acl.playground_private_acl.id}"
-  egress = false
-  protocol = "tcp"
-  rule_number = 104
-  rule_action = "allow"
-  cidr_block = "0.0.0.0/0"
-  from_port = 1024
-  to_port = 65535
+  network_acl_id = aws_network_acl.playground_private_acl.id
+  egress         = false
+  protocol       = "tcp"
+  rule_number    = 104
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 1024
+  to_port        = 65535
 }
 
 # Allow ingress from the bastion via ssh
 resource "aws_network_acl_rule" "private_ingress_from_bastion_via_ssh" {
-  network_acl_id = "${aws_network_acl.playground_private_acl.id}"
-  egress = false
-  protocol = "tcp"
-  rule_number = 110
-  rule_action = "allow"
-  cidr_block = "${aws_instance.playground_bastion.private_ip}/32"
-  from_port = 22
-  to_port = 22
+  network_acl_id = aws_network_acl.playground_private_acl.id
+  egress         = false
+  protocol       = "tcp"
+  rule_number    = 110
+  rule_action    = "allow"
+  cidr_block     = "${aws_instance.playground_bastion.private_ip}/32"
+  from_port      = 22
+  to_port        = 22
 }
 
 # Allow egress to the bastion via ephemeral ports
 resource "aws_network_acl_rule" "private_egress_to_bastion_via_ephemeral_ports" {
-  network_acl_id = "${aws_network_acl.playground_private_acl.id}"
-  egress = true
-  protocol = "tcp"
-  rule_number = 111
-  rule_action = "allow"
-  cidr_block = "${aws_instance.playground_bastion.private_ip}/32"
-  from_port = 1024
-  to_port = 65535
+  network_acl_id = aws_network_acl.playground_private_acl.id
+  egress         = true
+  protocol       = "tcp"
+  rule_number    = 111
+  rule_action    = "allow"
+  cidr_block     = "${aws_instance.playground_bastion.private_ip}/32"
+  from_port      = 1024
+  to_port        = 65535
 }
+
